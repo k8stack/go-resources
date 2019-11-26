739c34436e5cd0a620eaecffd0549f24e1ab9225
zillani <shaikzillani@gmail.com>
2019-11-26 15:14:07 +0530

    update

---
+++ linkedlist/understandRecursionTrick.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	do(1)
+}
+
+//understand how this traverses backwards, the same logic is applied for printing the linkedlist in reverse order, 
+//only after the recrusion is complete, the statements after it are executed.
+func do(n int) int {
+	fmt.Print("loop")
+	if n == 5 {
+		return 5
+	}
+	do(n+1)
+	fmt.Println(n)
+	return n
+}
