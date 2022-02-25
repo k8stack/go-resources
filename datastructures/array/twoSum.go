de128883cd0d12332fb0a708f2e4f592f1cac68f
zshaik <shaikzillani@gmail.com>
2022-02-25 14:24:02 +0530

    Update twoSum.go

--- datastructures/array/twoSum.go
+++ datastructures/array/twoSum.go
@@ -10,10 +10,14 @@ func main() {
 }
 
 func twoSums(s []int, target int) []int {
+	// put array into a map like,
+	// | 2, 0 | 7, 1 | 11, 3 | 15, 4 |
 	m := make(map[int]int)
 	for i := 0; i < len(s); i++ {
 		m[s[i]] = i
 	}
+	// substract target from array each value 
+	// if the result exists in the map then return
 	for i := 0; i < len(s); i++ {
 		val, isExist := m[target-s[i]]
 
